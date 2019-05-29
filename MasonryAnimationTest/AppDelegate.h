8d4624e5bc06524dda7442ad07aa6825b3047f5e
郭冬冬 <guodongdong@inke.cn>
2019-05-29 21:11:02 +0800

    Initial Commit

---
+++ MasonryAnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryAnimationTest
+//
+//  Created by guodongdong on 2019/5/29.
+//  Copyright © 2019 guodongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryAnimationTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MasonryAnimationTest
+//
+//  Created by guodongdong on 2019/5/29.
+//  Copyright © 2019 guodongdong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
